5d9f3e12b4a2d12279f0eda912cf5cde629c728a
Tsuda12 <felipe.tsuda01@gmail.com>
2024-11-07 22:29:48 -0300

    init: start project

---
+++ src/main/java/br/com/tsuda/med_clinic_api/MedClinicApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tsuda.med_clinic_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedClinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedClinicApiApplication.class, args);
+	}
+
+}
