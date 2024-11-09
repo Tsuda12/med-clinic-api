9cff02f96883fd3361bfc803e1a52dae31083616
Tsuda12 <frt.120304@gmail.com>
2024-11-09 14:48:54 -0300

    refactor: add response entity in endpoints responses

--- src/main/java/br/com/tsuda/med_clinic_api/controller/MedicController.java
+++ src/main/java/br/com/tsuda/med_clinic_api/controller/MedicController.java
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medics")
@@ -22,26 +24,36 @@ public class MedicController {
 
     @PostMapping
     @Operation(summary = "Create Medic")
-    public MedicResponseDTO create(@RequestBody @Valid MedicRequestDTO request) {
-        return medicService.create(request);
+    public ResponseEntity<MedicResponseDTO> create(@RequestBody @Valid MedicRequestDTO request, UriComponentsBuilder uriBuilder) {
+        MedicResponseDTO response = medicService.create(request);
+
+        var uri = uriBuilder.path("/medics/{id}").buildAndExpand(response.id).toUri();
+
+        return ResponseEntity.created(uri).body(response);
     }
 
     @GetMapping
     @Operation(summary = "Get all Medics")
-    public Page<MedicResponseDTO> getAll(@PageableDefault(size = 10, page = 0, sort = {"name"}, direction = Sort.Direction.ASC)
+    public ResponseEntity<Page<MedicResponseDTO>> getAll(@PageableDefault(size = 10, page = 0, sort = {"name"}, direction = Sort.Direction.ASC)
                                          Pageable pagination) {
-        return medicService.getAll(pagination);
+        Page<MedicResponseDTO> response = medicService.getAll(pagination);
+
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping
     @Operation(summary = "Update Medic by id")
-    public MedicResponseDTO update(@RequestBody @Valid MedicUpdateRequestDTO request) {
-        return medicService.update(request);
+    public ResponseEntity<MedicResponseDTO> update(@RequestBody @Valid MedicUpdateRequestDTO request) {
+        MedicResponseDTO response = medicService.update(request);
+
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete Medic by id")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity delete(@PathVariable Long id) {
         medicService.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
