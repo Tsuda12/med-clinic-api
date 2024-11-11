213da0fe85d2784720306871ca102efd055a5a32
Tsuda12 <frt.120304@gmail.com>
2024-11-11 10:30:29 -0300

    feat: add exception handler error 404

---
+++ src/main/java/br/com/tsuda/med_clinic_api/infra/ControllerExceptionHandler.java
@@ -0,0 +1,15 @@
+package br.com.tsuda.med_clinic_api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404() {
+        return ResponseEntity.notFound().build();
+    }
+}
